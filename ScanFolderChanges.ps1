2698b16a81911e8ea4da6a4329427ae5230a3e5d
Rudolf Borić <Rudolf.Boric@adacta-group.com>
2019-04-12 14:27:13 +0200

    Added hash table for .txt content

--- ScanFolderChanges.ps1
+++ ScanFolderChanges.ps1
@@ -15,18 +15,56 @@ while($true){
 
     Write-Host "Change in " + $result.Name
 
+    $TxtFile = $result.Name
     $NewName = get-date -Format 'yyyyMMddTHHmmss'
 
-    $Contents = Get-Content "D:\backup\*.txt"
-
+    $Contents = Get-Content "D:\backup\$TxtFile"
+    #$Contents = [io.file]::ReadAllLines("D:\backup\$TxtFile")
+    <#
     $source = $Contents[0].split("=")[1]
     $module = $Contents[1].split("=")[1]
     $action = $Contents[2].split("=")[1]
     $mail   = $Contents[3].split("=")[1]
+#>
+    $FileContent = @{}
 
-    
-    & $ScriptPath -Source $source -Module $module -Action $action -Mail $mail
+    foreach ($Line in $Contents){
+        $FileContent[$Line.split('=').trim()[0]] = ($Line.split('=').trim()[1])
+    }
 
-    Copy-Item -Path 'D:\backup\*.txt' -Destination "D:\Stuff\$NewName" -Force -Verbose
-    Remove-item -Path 'D:\backup\*.txt'
+    Write-Host "Source after split: " $FileContent["module"]
+    #& $ScriptPath -Source $source -Module $module -Action $action -Mail $mail
+
+    & $ScriptPath -Source $FileContent["source"] -Module $FileContent["module"] -Action $FileContent["action"] -Mail $FileContent["mail"]
+
+    Copy-Item -Path D:\backup\$TxtFile -Destination D:\Stuff\$NewName.txt -Force -Verbose
+    Remove-item -Path D:\backup\$TxtFile
 }
+
+<#
+hashtable
+i onda
+recimo hashtable nek se zove params
+$params = @{};
+i provrtis po linijama petlju
+i unutar petlje
+$params[...split[0]] = ...split[1]
+i onda dok zoves skriptu ces imat
+npr. scriptpath -Module $params["module"] -....
+itd.
+
+Pa ono
+Table["ruda"]=555
+I tak
+Tak on radi
+Mozes mu string dat ko indeks
+Tak bi ti mogo foreach
+Po redovima fajla
+Vrtit
+I ono
+table[dio prije znaka =] = dio poslje znaka =
+Dio prije znaka = je ono split 0
+A poslje je split 1
+Ono kak si napravio
+Split
+#>
